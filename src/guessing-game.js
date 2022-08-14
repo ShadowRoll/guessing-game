28590b2261ce04ecf767d48a9734d63c741d3de9
Ilya <70850776+ShadowRoll@users.noreply.github.com>
2022-08-14 21:09:07 +0300

    feat: added solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
     constructor() {}
-
+    
     setRange(min, max) {
-
+    this.min = min;
+    this.max = max;
     }
 
     guess() {
-
+        //console.log(Math.round((this.min + this.max)/2))
+        return Math.round((this.min + this.max)/2);
     }
 
     lower() {
 
+    this.max = this.guess();
+    //console.log(`lower is ${this.min}`)
     }
 
     greater() {
-
+   this.min = this.guess();
+   //console.log(`greater is ${this.min}`)
     }
+    
 }
 
 module.exports = GuessingGame;
